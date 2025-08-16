5757614bae8fe2b79d96c9dddacd2e8737066713
hanan1hub <h.hanan2005hb@gmail.com>
2025-08-16 14:34:46 +0500

    X and Diamond in one

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,64 @@
 # include <iostream>
 using namespace std;
 int main(){
-    cout<<"hello world"<<endl;
+    
+    int x;
+    cout<<"enter no of rows: ";
+    cin>>x;
+
+    for (int i = 0;i< x ; i++){
+        for(int k =x;k>x-i;k--){
+            cout<<" ";
+        }
+        for(int j =1;j<=x-i;j++){
+        
+            cout<<"* ";
+        }
+    
+        cout<<endl;
+
+    }
+
+    for (int i = 2;i<= x ; i++){
+        for(int k =x-i;k>0;k--){
+            cout<<" ";
+        }
+        for(int j =0;j<i;j++){
+        
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+
+
+    cout<<endl;
+    cout<<endl;
+
+     for (int i = 1;i<= x ; i++){
+        for(int k =x-i;k>0;k--){
+            cout<<" ";
+        }
+        for(int j =0;j<i;j++){
+        
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+
+    for (int i = 1;i< x ; i++){
+        for(int k =x;k>x-i;k--){
+            cout<<" ";
+        }
+        for(int j =1;j<=x-i;j++){
+        
+            cout<<"* ";
+        }
+    
+        cout<<endl;
+
+    }
+
     return 0;
 }
