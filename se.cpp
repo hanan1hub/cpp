41c6f677ea6c63835994efef89e7d1478b918682
hanan1hub <h.hanan2005hb@gmail.com>
2025-08-19 23:34:27 +0500

    d

---
+++ se.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main(){
+
+   int array[]={2,3,2,54,65};
+
+   int length = sizeof(array)/sizeof(array[0]);
+
+   for(int i =0;i<length;i++){
+    if(array[i]<array[i+1]){
+     
+
+    }
+
+
+
+   }
+
+    return 0;
+}
